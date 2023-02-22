1d4ef096bcd47d87782954365e3807e7a5919b1e
Eli Lurie <ehl8022@rit.edu>
2023-02-21 20:46:45 -0500

    Implemented getProduct in the ProductController

--- estore-api/src/main/controller/ProductController.java
+++ estore-api/src/main/controller/ProductController.java
@@ -1,5 +1,10 @@
 package com.estore.api.estoreapi;
 
+import java.io.IOError;
+import java.io.IOException;
+import java.util.logging.Level;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.estore.api.estoreapi.persistence.ProductDAO;
 
 @RestController
 @RequestMapping("products")
@@ -22,9 +29,29 @@ public class ProductController {
         this.productDAO = productDAO;
     }
 
+
+    /**
+     * Responds to the GET request for a product for the given id
+     * 
+     * @param id    The id used to locate the product
+     * @return      ResponseEntity with the product object and HTTP status of OK if found
+     * ResponseEntity with HTTP status of NOT_FOUND if not found
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */    
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProduct(@PathVariable int id) {
-
+        LOG.info("GET /products/" + id);
+        try {
+            Product product = productDAO.getProduct(id);
+            if(product != null)
+                return new ResponseEntity<Product>(product, HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("")
