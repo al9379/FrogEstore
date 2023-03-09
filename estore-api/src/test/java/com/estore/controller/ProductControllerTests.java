44b2a49278c5036b5e33321562e0cbc7565b3cfe
al9379 <113538339+al9379@users.noreply.github.com>
2023-03-08 19:38:54 -0500

    ControlTests

--- estore-api/src/test/java/com/estore/controller/ProductControllerTests.java
+++ estore-api/src/test/java/com/estore/controller/ProductControllerTests.java
@@ -1,5 +1,230 @@
 package com.estore.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import com.estore.api.estoreapi.persistence.ProductDAO;
+
+import net.bytebuddy.agent.VirtualMachine.ForHotSpot.Connection.Response;
+
+import com.estore.api.estoreapi.controller.ProductController;
+import com.estore.api.estoreapi.model.Product;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
+@Tag("Controller-Tier")
 public class ProductControllerTests {
-    
+    private ProductController productController;
+    private ProductDAO mockProductDAO;
+
+    @BeforeEach
+    public void setupProductController(){
+        mockProductDAO = mock(ProductDAO.class);
+        productController = new ProductController(mockProductDAO);
+    }
+
+    @Test
+    public void testGetProduct() throws IOException{
+        Product product = new Product(1,10,15,"tree","poison");
+
+        when(mockProductDAO.getProduct(product.getId())).thenReturn(product);
+
+        ResponseEntity<Product> response = productController.getProduct(product.getId());
+
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals(product,response.getBody());
+    }
+
+    @Test
+    public void testGetProductsHandleException() throws Exception{
+        int productId = 1;
+        doThrow(new IOException()).when(mockProductDAO).getProduct(productId);
+
+        ResponseEntity<Product> response = productController.getProduct(productId);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
+    }
+
+    @Test
+    public void testGetProductNotFound() throws Exception{
+        int productId = 1;
+
+        when(mockProductDAO.getProduct(productId)).thenReturn(null);
+
+        ResponseEntity<Product> response = productController.getProduct(productId);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    public void testCreateProduct() throws IOException{
+        Product product = new Product(1,10,15,"tree","poison");
+
+        when(mockProductDAO.createProduct(product)).thenReturn(product);
+
+        ResponseEntity<Product> response = productController.createProduct(product);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(product, response.getBody());
+    }
+
+    @Test
+    public void testCreateProductFailed() throws IOException{
+        Product product = new Product(1,10,15,"tree","poison");
+
+        when(mockProductDAO.createProduct(product)).thenReturn(null);
+
+        ResponseEntity<Product> response = productController.createProduct(product);
+
+        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+    }
+
+    @Test
+    public void testUpdateProduct() throws IOException{
+        Product product = new Product(1,10,15,"tree","poison");
+
+        when(mockProductDAO.updateProduct(product)).thenReturn(product);
+
+        ResponseEntity<Product> response = productController.updateProduct(product);
+        product.setName("bush");
+
+        response = productController.updateProduct(product);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(product, response.getBody());
+    }
+
+    @Test
+    public void testUpdateProductFailed() throws IOException{
+        Product product = new Product(1,10,15,"tree","poison");
+
+        when(mockProductDAO.updateProduct(product)).thenReturn(null);
+
+        ResponseEntity<Product> response = productController.updateProduct(product);
+
+        assertEquals(HttpStatus.NOT_FOUND,response.getStatusCode());
+    }
+
+    @Test
+    public void testUpdateProductHandleException() throws IOException{
+
+        Product product = new Product(1,10,15,"tree","poison");
+
+        doThrow(new IOException()).when(mockProductDAO).updateProduct(product);
+
+        ResponseEntity<Product> response = productController.updateProduct(product);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
+    }
+
+    @Test
+    public void testGetAll() throws IOException { // getHeroes may throw IOException
+        // Setup
+        Product[] products = new Product[2];
+        products[0] = new Product(1,10,15,"tree","poison");
+        products[1] = new Product(2,8,10,"bull","burp");
+        
+        when(mockProductDAO.getAll()).thenReturn(products);
+
+        // Invoke
+        ResponseEntity<Product[]> response = productController.getProducts();
+
+        // Analyze
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals(products,response.getBody());
+    }
+
+    @Test
+    public void testGetAllHandleException() throws IOException {
+        // Setup
+        
+        doThrow(new IOException()).when(mockProductDAO).getAll();
+
+        // Invoke
+        ResponseEntity<Product[]> response = productController.getProducts();
+
+        // Analyze
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
+    }
+
+    @Test
+    public void testSearchFor() throws IOException { 
+        // Setup
+        String searchString = "re";
+        Product[] products = new Product[2];
+        products[0] = new Product(1,10,15,"tree","poison");
+        products[1] = new Product(2,8,10,"bull","burp");
+       
+        when(mockProductDAO.searchFor(searchString)).thenReturn(products);
+
+        // Invoke
+        ResponseEntity<Product[]> response = productController.searchProducts(searchString);
+
+        // Analyze
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals(products,response.getBody());
+    }
+
+    @Test
+    public void testSearchForHandleException() throws IOException { 
+        // Setup
+        String searchString = "an";
+        
+        doThrow(new IOException()).when(mockProductDAO).searchFor(searchString);
+
+        // Invoke
+        ResponseEntity<Product[]> response = productController.searchProducts(searchString);
+
+        // Analyze
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
+    }
+
+    @Test
+    public void testDeleteProduct() throws IOException { 
+        // Setup
+        int productId = 1;
+        
+        when(mockProductDAO.deleteProduct(productId)).thenReturn(true);
+
+        ResponseEntity<Product> response = productController.deleteProduct(productId);
+
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+    }
+
+    @Test
+    public void testDeleteHeroNotFound() throws IOException { 
+        // Setup
+        int productId = 1;
+       
+        when(mockProductDAO.deleteProduct(productId)).thenReturn(false);
+
+        // Invoke
+        ResponseEntity<Product> response = productController.deleteProduct(productId);
+
+        // Analyze
+        assertEquals(HttpStatus.NOT_FOUND,response.getStatusCode());
+    }
+
+    @Test
+    public void testDeleteHeroHandleException() throws IOException { 
+        // Setup
+        int productId = 1;
+        
+        doThrow(new IOException()).when(mockProductDAO).deleteProduct(productId);
+
+        // Invoke
+        ResponseEntity<Product> response = productController.deleteProduct(productId);
+
+        // Analyze
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
+    }
 }
--- estore-api/src/test/java/com/estore/controller/UserControllerTests.java
+++ estore-api/src/test/java/com/estore/controller/UserControllerTests.java
@@ -1,5 +1,120 @@
 package com.estore.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import com.estore.api.estoreapi.persistence.UserDAO;
+
+import net.bytebuddy.agent.VirtualMachine.ForHotSpot.Connection.Response;
+
+import com.estore.api.estoreapi.controller.UserController;
+import com.estore.api.estoreapi.model.ShoppingCart;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Tag("Controller-Tier")
 public class UserControllerTests {
-    
+    private UserController userController;
+    private UserDAO mockUserDAO;
+
+    @Test
+    public void testCreateUser() throws IOException{
+        String user = "Tim";
+
+        when(mockUserDAO.addUser(user)).thenReturn(true);
+
+        ResponseEntity<String> response = userController.createUser(user);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(user, response.getBody());
+    }
+
+    @Test
+    public void testCreateProductFailed() throws IOException{
+        String user = "Tim";
+
+        when(mockUserDAO.addUser(user)).thenReturn(null);
+
+        ResponseEntity<String> response = userController.createUser(user);
+
+        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+    }
+
+    @Test
+    public void testGetAll() throws IOException {
+        // Setup
+        String[] users = new String[2];
+        users[0] = new String("Tim");
+        users[1] = new String("Drake");
+
+        when(mockUserDAO.getAll()).thenReturn(users);
+
+        // Invoke
+        ResponseEntity<String[]> response = userController.getUsers();
+
+        // Analyze
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals(users,response.getBody());
+    }
+
+    @Test
+    public void testGetAllHandleException() throws IOException { 
+        // Setup
+       
+        doThrow(new IOException()).when(mockUserDAO).getAll();
+
+        // Invoke
+        ResponseEntity<String[]> response = userController.getUsers();
+
+        // Analyze
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
+    }
+
+    @Test
+    public void testDeleteUser() throws IOException { 
+        // Setup
+        String user = "Tim";
+        
+        when(mockUserDAO.deleteUser(user)).thenReturn(true);
+
+        ResponseEntity<String> response = userController.deleteUser(user);
+
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+    }
+
+    @Test
+    public void testDeleteHeroNotFound() throws IOException { 
+        // Setup
+        String user = "Tim";
+        
+        when(mockUserDAO.deleteUser(user)).thenReturn(false);
+
+        // Invoke
+        ResponseEntity<String> response = userController.deleteUser(user);
+
+        // Analyze
+        assertEquals(HttpStatus.NOT_FOUND,response.getStatusCode());
+    }
+
+    @Test
+    public void testDeleteHeroHandleException() throws IOException { 
+        // Setup
+        String user = "Tim";
+        
+        doThrow(new IOException()).when(mockUserDAO).deleteUser(user);
+
+        // Invoke
+        ResponseEntity<String> response = userController.deleteUser(user);
+
+        // Analyze
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
+    }
 }
