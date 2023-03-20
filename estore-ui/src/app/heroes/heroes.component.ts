d623d00ae90b05bcc1e381efe3b7dc4ffa0c5c13
Alex <al9379@rit.edu>
2023-03-20 17:46:12 -0400

    productdetail

--- estore-ui/src/app/heroes/heroes.component.ts
+++ estore-ui/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Hero } from '../hero';
+import { Hero } from '../product';
 import { HeroService } from '../hero.service';
 
 @Component({
---
+++ estore-ui/src/app/product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+  id: number;
+  stock: number;
+  price: number;
+  name: String;
+  description: String;
+}
