c9708c49ebfc7aa31a967f4dc7b94829087baa61
Eli Lurie <ehl8022@rit.edu>
2023-03-21 22:08:09 -0400

    Added documentation to UserDAO

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserDAO.java
@@ -3,8 +3,33 @@ package com.estore.api.estoreapi.persistence;
 import java.io.IOException;
 
 public interface UserDAO {
-    public String[] getAll() throws IOException;
-    public boolean addUser(String username) throws IOException;
-    public boolean deleteUser(String username) throws IOException;
-    public boolean userExists(String username);
+    /**
+     * Retrieves all current users
+     * @return Array of username
+     * @throws IOException if an issue with storage
+     */
+    String[] getAll() throws IOException;
+
+    /**
+     * Creates and saves a new user with the given username
+     * @param username username of the new user
+     * @return true if successful, false otherwise
+     * @throws IOException if an issue with storage
+     */
+    boolean addUser(String username) throws IOException;
+
+    /**
+     * Deletes a user with the given username
+     * @param username username of the user to delete
+     * @return true if successful, false otherwise
+     * @throws IOException if an issue with storage
+     */
+    boolean deleteUser(String username) throws IOException;
+
+    /**
+     * Checks if a user with the given username exists
+     * @param username the username of the user
+     * @return true if the user exists, false otherwise
+     */
+    boolean userExists(String username);
 }
