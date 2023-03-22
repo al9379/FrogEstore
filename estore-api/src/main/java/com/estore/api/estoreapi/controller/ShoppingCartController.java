980eebcedc55f6a8b755884a59d959f63c66d309
Eli Lurie <ehl8022@rit.edu>
2023-03-21 21:54:28 -0400

    Added documentation to ShoppingCartController

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/ShoppingCartController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/ShoppingCartController.java
@@ -3,6 +3,7 @@ package com.estore.api.estoreapi.controller;
 import java.io.IOException;
 import java.util.logging.Level;
 
+import com.estore.api.estoreapi.model.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,7 +21,7 @@ import com.estore.api.estoreapi.persistence.ShoppingCartDAO;
 import java.util.logging.Logger;
 
 /**
- * Handles the REST API requests for the Product resource
+ * Handles the REST API requests for the Shopping Cart resource
  */
 
 @RestController
@@ -29,10 +30,23 @@ public class ShoppingCartController {
     private static final Logger LOG = Logger.getLogger(ShoppingCartController.class.getName());
     private ShoppingCartDAO shoppingCartDAO;
 
+    /**
+     * Creates a REST API controller which responds to requests
+     * @param shoppingCartDAO The {@link ShoppingCartDAO Shopping Cart Data Access Object} to
+     *                        perform CRUD operations
+     */
     public ShoppingCartController(ShoppingCartDAO shoppingCartDAO) {
         this.shoppingCartDAO = shoppingCartDAO;
     }
 
+    /**
+     * Responds to the GET request for a shopping cart for the given username
+     * @param username The username used to locate the shopping cart
+     * @return ResponseEntity with the shopping cart object and HTTP status of OK if
+     *         found<br>
+     *         ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     *         ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
    @GetMapping("/{username}")
     public ResponseEntity<ShoppingCart> getCart(@PathVariable String username) {
         LOG.info("GET /carts/" + username);
@@ -48,7 +62,18 @@ public class ShoppingCartController {
         }
     }
 
-   @PostMapping("/{username}")
+    /**
+     * Creates a shopping cart with the provided username
+     *
+     * @param username - The username of the shopping cart to create
+     *
+     * @return ResponseEntity with given username and HTTP status
+     *         of CREATED<br>
+     *         ResponseEntity with HTTP status of CONFLICT if a shopping cart
+     *         object with that username already exists<br>
+     *         ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @PostMapping("/{username}")
     public ResponseEntity<String> createShoppingCart(@PathVariable String username) {
         LOG.info("POST /carts" + username);
         try {
@@ -65,7 +90,17 @@ public class ShoppingCartController {
         }
     }
 
-   @PutMapping("/{username}")
+    /**
+     * Updates the shopping cart with the provided shopping cart object, if it exists
+     *
+     * @param shoppingCart The shopping cart to update
+     *
+     * @return ResponseEntity with the username of the updated shopping cart object and HTTP status
+     *         of OK if updated<br>
+     *         ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     *         ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @PutMapping("/{username}")
     public ResponseEntity<String> updateCart(@RequestBody ShoppingCart shoppingCart) {
         LOG.info("PUT /carts" + shoppingCart);
         try {
@@ -82,6 +117,15 @@ public class ShoppingCartController {
         }
     }
 
+    /**
+     * Deletes a {@linkplain ShoppingCart shopping cart} with the given username
+     *
+     * @param username The username of the {@link ShoppingCart shopping cart} to deleted
+     *
+     * @return ResponseEntity HTTP status of OK if deleted<br>
+     *         ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     *         ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
     @DeleteMapping("/{username}")
     public ResponseEntity<String> deleteShoppingCart(@PathVariable String username) {
         LOG.info("DELETE /carts/" + username);
