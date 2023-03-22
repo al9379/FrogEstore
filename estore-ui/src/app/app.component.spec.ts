560f8c88cbd18d79924c4183a9672ed92e86fd0b
Eli Lurie <ehl8022@rit.edu>
2023-03-21 22:24:41 -0400

    Updated title

--- estore-ui/src/app/app.component.spec.ts
+++ estore-ui/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tour-of-heroes'`, () => {
+  it(`should have as title 'e-store'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tour-of-heroes');
+    expect(app.title).toEqual('e-store');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tour-of-heroes app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('e-store app is running!');
   });
 });
